c1e191ac681ae0f5419baa5abe57aaa2c2bd3d75
donghun-han <donghun.han@kaist.ac.kr>
2023-06-24 22:05:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Shell Script Files for One-line Installation
+## Usage
+```shell
+git clone https://github.com/donghun-han/easy_install.git
+cd easy_install/scripts
+chmod +x ./*
+./[file_name.sh]
+```
---
+++ scripts/ros_noetic_install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ROS Noetic Installation
+
+# Step 1 : Configure your Ubuntu repositories
+sudo add-apt-repository restricted
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
+
+sudo apt update
+
+# Step 2: Setup your source.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# Step 3: Setup your keys
+sudo apt install -y curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+# Step 4: Install ROS Noetic Desktop-full
+sudo apt update
+sudo apt install -y ros-noetic-desktop-full
+sudo apt install -y ros-noetic-rqt*
+
+# Step 5: Setup environment
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo rosdep init
+rosdep update
+
+# Step 6: Verifying ROS Version
+source ~/.bashrc
+rosversion -d
